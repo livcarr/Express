93844131b3fbee7f9c93662f4a9ce02a2ee7ea87
Kay Langfitt <langfittkr@appstate.edu>
2025-02-12 15:44:22 -0500

    targetSdk back to 34; compileSdk still at 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.bignerdranch.express"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
