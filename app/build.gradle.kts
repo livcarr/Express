c7893c3297170a95bf4a95e96fc41e454df3daa7
Kay Langfitt <langfittkr@appstate.edu>
2025-02-12 15:36:24 -0500

    changes to targetSdk and compileSdk in Gradle Module:app file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.bignerdranch.express"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.bignerdranch.express"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
